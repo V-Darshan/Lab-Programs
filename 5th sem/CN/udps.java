66e931a2cfb95e9a65a186813995a694679d1569
V-Darshan <1mp23ad053@bgscet.ac.in>
2025-11-13 10:08:40 +0530

    update

--- 5th sem/CN/udps.java
+++ 5th sem/CN/udps.java
@@ -1,27 +1,33 @@
-import java.io.*;
-importjava.net.*;
-importjava.util.*;
-public class udps
-{
-publicstaticvoid main(String[]args)
-{
-DatagramSocketskt=null;
-Scannersc=newScanner(System.in); try
-{
-skt=newDatagramSocket(2400); byte[] buffer=new
-byte[1000]; while(true)
-{
-DatagramPacketrequest=newDatagramPacket(buffer,buffer.length);
-skt.receive(request);
-Stringmessage=sc.nextLine();
-byte[]sendMsg=message.getByte
-s(); DatagramPacket reply=new
-DatagramPacket(sendMsg,sendMsg.length,request.getAddress(),request.getPort());
-skt.send(reply);
-}
-}
-catch(Exceptionex)
-{
-}
-}
+import java.net.*;
+import java.util.Scanner;
+
+public class udps {
+	public static void main(String[] args) {
+		DatagramSocket skt = null;
+		Scanner sc = new Scanner(System.in);
+		try {
+			skt = new DatagramSocket(2400);
+			byte[] buffer = new byte[1000];
+
+			while (true) {
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				skt.receive(request);
+
+				String received = new String(request.getData(), 0, request.getLength());
+				System.out.println("server received: " + received);
+
+				System.out.print("Reply: ");
+				String message = sc.nextLine();
+				byte[] sendMsg = message.getBytes();
+
+				DatagramPacket reply = new DatagramPacket(sendMsg, sendMsg.length, request.getAddress(), request.getPort());
+				skt.send(reply);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (skt != null) skt.close();
+			sc.close();
+		}
+	}
 }
